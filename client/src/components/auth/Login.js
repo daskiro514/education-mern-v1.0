19b47c018332cf372a342ebd7e0c481cd2928cf7
daskiro514 <luckybuzstar@gmail.com>
2021-12-09 08:57:00 +0900

    updated

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,12 @@
-import React, { Fragment, useState } from 'react'
+import React from 'react'
 import { Link, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { login } from '../../actions/auth'
+import logo from '../../img/logo.PNG'
 
 const Login = ({ login, isAuthenticated }) => {
-  const [formData, setFormData] = useState({
+  const [formData, setFormData] = React.useState({
     email: '',
     password: ''
   })
@@ -21,42 +22,84 @@ const Login = ({ login, isAuthenticated }) => {
   }
 
   if (isAuthenticated) {
-    return <Redirect to="/dashboard" />
+    return <Redirect to='/dashboard' />
   }
 
   return (
-    <Fragment>
-      <h1 className="large text-primary">Sign In</h1>
-      <p className="lead">
-        <i className="fas fa-user" /> Sign Into Your Account
-      </p>
-      <form className="form" onSubmit={onSubmit}>
-        <div className="form-group">
-          <input
-            type="email"
-            placeholder="Email Address"
-            name="email"
-            value={email}
-            onChange={onChange}
-            required
-          />
+    <div className='container-fluid bg-login'>
+      <div className='row'>
+        <div className='col-md-2'></div>
+        <div className='col-md-8'>
+          <br />
+          <br />
+          <br />
+          <br />
+          <div className='text-center'>
+            <img alt="SETIMG" src={logo} className='img-fluid' />
+          </div>
         </div>
-        <div className="form-group">
-          <input
-            type="password"
-            placeholder="Password"
-            name="password"
-            value={password}
-            onChange={onChange}
-            minLength="6"
-          />
+        <div className='col-md-2'></div>
+      </div>
+      <div className='row'>
+        <div className='col-lg-4 col-md-3'></div>
+        <div className='col-lg-4 col-md-6'>
+          <div className='row'>
+            <div className='col-1'></div>
+            <div className='col-10'>
+              <div className='row' style={{ height: '5%' }}></div>
+              <div className='row height-center'>
+                <div className='col'>
+                  <form className='form' onSubmit={onSubmit}>
+                    <div className='form-group'>
+                      <input
+                        type='email'
+                        placeholder='email'
+                        name='email'
+                        className='form-control'
+                        value={email}
+                        onChange={onChange}
+                        required
+                      />
+                    </div>
+                    <div className='form-group'>
+                      <input
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        className='form-control'
+                        value={password}
+                        onChange={onChange}
+                        minLength='6'
+                      />
+                    </div>
+                    <Link to="/forgotPassword" className="form-label pb-2">Forgot password?</Link>
+                    <div className='form-group'>
+                      <input
+                        type='submit'
+                        className='form-control btn'
+                        style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}
+                        value='Login'
+                      />
+                    </div>
+                    <div className='form-group'>
+                      <Link
+                        to='/register'
+                        className='btn form-control'
+                        style={{ backgroundColor: 'rgba(255, 255, 255, 0.7)' }}
+                      >
+                        Register
+                      </Link>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </div>
+            <div className='col-1'></div>
+          </div>
         </div>
-        <input type="submit" className="btn btn-primary" value="Login" />
-      </form>
-      <p className="my-1">
-        Don't have an account? <Link to="/register">Sign Up</Link>
-      </p>
-    </Fragment>
+        <div className='col-lg-4 col-md-3'></div>
+      </div>
+    </div>
   )
 }
 
--- client/src/components/customer/CustomerDashboard.js
+++ client/src/components/customer/CustomerDashboard.js
@@ -1,11 +1,52 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Vimeo from '@u-wave/react-vimeo'
 
-const CustomerDashboard = () => {
+const CustomerAcademy = () => {
 
   return (
-    <div>
-      Customer Dashboard
+    <div className='row Customer-academy bg-pure-gold-grey py-4'>
+      <div className='col-lg-12'>
+        <div className='bg-white pure-gold-rounded-lg p-3 mb-3'>
+          <div className='font-24 font-bold'>Academy <i className='fa fa-plus-circle text-pure-gold-grey'></i></div>
+          <div className='row pt-4'>
+            <div className='col-lg-3 col-md-5 border-right'>
+              {['READY', 'SET', 'LAUNCH'].map((item, index) =>
+                <div key={index} className='pb-4'>
+                  <div className='font-18 font-bold'>
+                    {item} <i className='fa fa-plus-circle text-pure-gold-grey cursor-pointer'></i>
+                  </div>
+                  {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((moduleItem, moduleIndex) =>
+                    <div key={moduleIndex} className={'rounded-lg cursor-pointer font-16 pl-1 ' + (index === 0 && moduleIndex === 0 ? 'bg-pure-gold-brown' : '')}>
+                      <i className='fa fa-caret-right'></i> Chapter {moduleItem}
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
+            <div className='col-lg-9 col-md-7'>
+              <div className='font-18 font-bold'>
+                Module 1: How To Setup An Online Wal-Mart Store
+              </div>
+              <div className='font-18 pt-3 text-justify'>
+                The Walmart Marketplace is a platform that allows third-party sellers to list their items on Walmart.com, just like you would on Amazon or eBay. Follow the unique link in your approval email (subject line: “Your Account has been created in Walmart Marketplace”) to create your Partner Profile.
+              </div>
+              <div className='font-18 pt-3 text-justify'>
+                The Registration Wizard will walk you through five main sections: Account Creation, Partner Registration, Taxes (W-9),  and Shipping Info. Now that you have created your Partner Profile, you will have access to the Seller Center. Here, you’ll find a “Launch Checklist” that outlines the next several requirements before you can launch your account.
+              </div>
+              <div className='font-18 pt-3 text-justify'>
+                The final step for selling on Walmart Marketplace is launching your account. When you “mark as done” all the items on your Walmart Launch Checklist in Seller Center, a pop-up message will ask you to confirm that you’re ready to launch. When you select “confirm”, this will trigger a launch request.
+              </div>
+              <div className="text-center p-1 pt-3">
+                <Vimeo
+                  video={354682480}
+                  responsive={true}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
@@ -14,4 +55,4 @@ const mapStateToProps = state => ({
 
 })
 
-export default connect(mapStateToProps, {})(CustomerDashboard)
+export default connect(mapStateToProps, {})(CustomerAcademy)
